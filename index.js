0df1e0b8ea7fbb1dfaf320cac886f96ae973303d
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-03-16 19:48:26 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    return 2000/100
+}
+const square = (x)=>x*x;
+let sum=function(){
+    return 'add';
+}
+let a=3
+let b=4
+let add = (parameterl1,parameter2 )=> parameterl1 + parameter2;
